ef2e005b0fd1bb8cefa4b349d4d3c2592e25ec40
adityakahb <aditya.bhakay@live.com>
2021-12-15 23:18:03 +0530

    Work on nx

---
+++ client/apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'client-categories-list',
+    templateUrl: './categories-list.component.html',
+    styleUrls: ['./categories-list.component.scss']
+})
+export class CategoriesListComponent implements OnInit {
+    categories = [
+        {
+            id: '1',
+            name: 'category-1',
+            icon: 'icon-1'
+        },
+        {
+            id: '2',
+            name: 'category-2',
+            icon: 'icon-2'
+        },
+        {
+            id: '3',
+            name: 'category-3',
+            icon: 'icon-3'
+        }
+    ];
+
+    constructor() {}
+
+    ngOnInit(): void {}
+}
